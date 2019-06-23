c9f2fe30296b88c5a2bc2a0135276d8bdeb50336
codecampNick <nick.jp.ross@gmail.com>
2019-06-23 08:06:49 -0600

    moving secrets from environment variables to file

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from . import secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('MYSITE2_KEY')
+#SECRET_KEY = os.environ.get('MYSITE2_KEY')
+SECRET_KEY = secret.s_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
