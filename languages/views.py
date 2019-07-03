63079030d2c2aa714056d646447d42a0100ef1ef
codecampNick <nick.jp.ross@gmail.com>
2019-07-02 18:23:18 -0600

    Work around languages

--- languages/views.py
+++ languages/views.py
@@ -5,7 +5,18 @@ from .models import *
 
 def index(request):
     langs = Language.objects.all().order_by('-experience_level')
-    return render(request, 'languages_home.html', {
-        'title': "Languages Home",
-        'langs': langs,
-    })
+    star_value = {}
+    for lang in langs:
+        star_percent = (lang.experience_level / 5) * 100
+        rounded = (round(star_percent / 10) * 10)
+        if lang.name in star_value:
+            continue
+        else:
+            star_value[lang.name] = rounded
+
+    return render(request,'languages_home.html',
+                  {
+                      'title': 'Languages Home',
+                      'langs': langs,
+                      'star_value': star_value,
+                  })
