4721ac0188b36cb17bde86f715493ad76ce65733
codecampNick <nick.jp.ross@gmail.com>
2019-09-10 09:49:37 -0600

    checking in work around contact form

---
+++ contact/views.py
@@ -0,0 +1,26 @@
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+
+def index(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message'] + '\n\ncontact via: ' + from_email
+            try:
+                send_mail(subject,message,'nick.jp.ross@gmail.com',['nick.jp.ross@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid Header Found')
+            return HttpResponseRedirect('success')
+    return render(request,'contact_home.html', {'form': form})
+
+
+def successView(request):
+    return render(request, 'success.html')
+    #return HttpResponse('<hi>Your message is on its way!</h1>')
