e946557ec465327fcd59aeba8d6292ca2b0b98af
codecampNick <nick.jp.ross@gmail.com>
2019-09-10 13:09:14 -0600

    small changes around contact form

--- contact/forms.py
+++ contact/forms.py
@@ -2,7 +2,12 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    from_email = forms.EmailField(label='Email', required=True,
+                                  widget=forms.TextInput(attrs={'placeholder': 'your.email@address.com',
+                                                                'size': '22'}))
+    subject = forms.CharField(required=True,
+                              widget=forms.TextInput(attrs={'placeholder': 'I\'d like to chat because.... :)',
+                                                            'size': '24'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': '<sarcasm>Pretty obivious isn\'t it?\n' +
+                                                            'I mean, wow.</sarcasm>','rows': '4'}), required=True)
 
