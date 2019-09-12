a0069492bd725eb58a7ca6068677aa8b755554c4
codecampNick <nick.jp.ross@gmail.com>
2019-09-12 10:59:07 -0600

    testing contact background image

--- contact/views.py
+++ contact/views.py
@@ -18,7 +18,7 @@ def index(request):
             except BadHeaderError:
                 return HttpResponse('Invalid Header Found')
             return HttpResponseRedirect('success')
-    return render(request,'contact_home.html', {'title': 'Contact Home','form': form})
+    return render(request,'contact_home.html', {'title': 'Contact Home','form': form, 'n': range(5)})
 
 
 def successView(request):
