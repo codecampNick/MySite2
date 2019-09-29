3c85af1a9ead00442c1304eaefe9ba3950947676
codecampNick <nick.jp.ross@gmail.com>
2019-09-29 10:23:37 -0600

    work on image for phone

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret.s_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = secret.hosts
