532c9c513b8940bba6e084d59342e937a4d35d05
NickRoss <nick.jp.ros@gmail.com>
2020-03-28 15:40:34 -0600

    work on employer page.

--- employment/static/js/employment.js
+++ employment/static/js/employment.js
@@ -2,7 +2,37 @@ function testingThis(){
     console.log("From a function!");
 }
 
+function toggleVisibility(id, textId){
+    let status = document.getElementById(id).style.display;
+    console.log(status);
+    if(status === "none"){
+        console.log("should display");
+        document.getElementById(id).style.display = "";
+        $(textId).html("show less&#8230;");
+    }
+    else{
+        console.log("should hide");
+        document.getElementById(id).style.display = "none";
+        $(textId).html("show more&#8230;");
+    }
+}
+
 $(document).ready(function(){
     //alert("from js folder");
     //testingThis();
+    $("#health-ecareers-header").click(function(){
+        toggleVisibility("health-ecareers-body", "#health-ecareers-header");
+    });
+
+    $("#dice-holdings-inc-header").click(function(){
+        toggleVisibility("dice-holdings-inc-body", "#dice-holdings-inc-header");
+    });
+
+    $("#ontargetjobs-header").click(function(){
+        toggleVisibility("ontargetjobs-body", "#ontargetjobs-header");
+    });
+
+    $("#aerios-direct-inc-header").click(function(){
+        toggleVisibility("aerios-direct-inc-body", "#aerios-direct-inc-header");
+    });
 });
