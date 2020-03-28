7238423b95e05bb29d989f6fd26b43484291104c
NickRoss <nick.jp.ros@gmail.com>
2020-03-28 16:32:32 -0600

    added fade in and out to employer page

--- employment/static/js/employment.js
+++ employment/static/js/employment.js
@@ -7,13 +7,40 @@ function toggleVisibility(id, textId){
     console.log(status);
     if(status === "none"){
         console.log("should display");
-        document.getElementById(id).style.display = "";
+        //document.getElementById(id).style.display = "";
+        let el = document.getElementById(id);
+        //
+        el.style.opacity = 0
+        el.style.display = '';
+        let op = 0.1;  // initial opacity
+        let timer = setInterval(function () {
+            if (op >= 1){
+                clearInterval(timer);
+            }
+            el.style.opacity = op;
+            el.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op += op * 0.1;
+        }, 20);
+        //
         $(textId).html("show less&#8230;");
     }
     else{
         console.log("should hide");
-        document.getElementById(id).style.display = "none";
         $(textId).html("show more&#8230;");
+        //document.getElementById(id).style.display = "none";
+        let el = document.getElementById(id);
+        //
+        let op = 1;  // initial opacity
+        let timer = setInterval(function () {
+            if (op <= 0.1){
+                clearInterval(timer);
+                el.style.display = 'none';
+            }
+            el.style.opacity = op;
+            el.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op -= op * 0.1;
+        }, 50);
+        //
     }
 }
 
