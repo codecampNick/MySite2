3e33af33fed6c91f29ff09296fc481e8f14db23d
NickRoss <nick.jp.ros@gmail.com>
2020-03-29 14:00:38 -0600

    updated fade in and out to employer page. don't need to target id's

--- employment/static/js/employment.js
+++ employment/static/js/employment.js
@@ -1,65 +1,38 @@
-function testingThis(){
-    console.log("From a function!");
-}
-
-function toggleVisibility(id, textId){
-    let status = document.getElementById(id).style.display;
-    console.log(status);
-    if(status === "none"){
-        console.log("should display");
-        //document.getElementById(id).style.display = "";
-        let el = document.getElementById(id);
-        //
-        el.style.opacity = 0
-        el.style.display = '';
-        let op = 0.1;  // initial opacity
-        let timer = setInterval(function () {
-            if (op >= 1){
-                clearInterval(timer);
-            }
-            el.style.opacity = op;
-            el.style.filter = 'alpha(opacity=' + op * 100 + ")";
-            op += op * 0.1;
-        }, 20);
-        //
-        $(textId).html("show less&#8230;");
-    }
-    else{
-        console.log("should hide");
-        $(textId).html("show more&#8230;");
-        //document.getElementById(id).style.display = "none";
-        let el = document.getElementById(id);
-        //
-        let op = 1;  // initial opacity
-        let timer = setInterval(function () {
-            if (op <= 0.1){
-                clearInterval(timer);
-                el.style.display = 'none';
-            }
-            el.style.opacity = op;
-            el.style.filter = 'alpha(opacity=' + op * 100 + ")";
-            op -= op * 0.1;
-        }, 50);
-        //
-    }
-}
-
 $(document).ready(function(){
-    //alert("from js folder");
-    //testingThis();
-    $("#health-ecareers-header").click(function(){
-        toggleVisibility("health-ecareers-body", "#health-ecareers-header");
-    });
 
-    $("#dice-holdings-inc-header").click(function(){
-        toggleVisibility("dice-holdings-inc-body", "#dice-holdings-inc-header");
-    });
-
-    $("#ontargetjobs-header").click(function(){
-        toggleVisibility("ontargetjobs-body", "#ontargetjobs-header");
-    });
-
-    $("#aerios-direct-inc-header").click(function(){
-        toggleVisibility("aerios-direct-inc-body", "#aerios-direct-inc-header");
-    });
+    document.getElementById("employment-container").addEventListener('click', function(event){
+        if(event.target.classList.contains('show-hide')){
+            let el = event.target.parentElement;
+            let nextEl = el.nextElementSibling;
+            let status = nextEl.style.display;
+            console.log(status);
+            if(status === 'none'){
+                nextEl.style.opacity = 0
+                nextEl.style.display = '';
+                let op = 0.1;  // initial opacity
+                let timer = setInterval(function () {
+                    if (op >= 1){
+                        clearInterval(timer);
+                    }
+                    nextEl.style.opacity = op;
+                    nextEl.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                    op += op * 0.1;
+                }, 20);
+                event.target.innerText = "(show less...)";
+            }
+            else{
+                event.target.innerText = "(show more...)"
+                let op = 1;  // initial opacity
+                let timer = setInterval(function () {
+                    if (op <= 0.1){
+                        clearInterval(timer);
+                        nextEl.style.display = 'none';
+                    }
+                    nextEl.style.opacity = op;
+                    nextEl.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                    op -= op * 0.1;
+                }, 50);
+            }
+        }
+    }, false);
 });
